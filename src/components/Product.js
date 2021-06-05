9c7bbc4732fa85c9ee80d1c5fa748bc12e30cbc8
ihposey12 <ihposey@crimson.ua.edu>
2021-06-05 16:12:09 -0700

    ContextAPI setup and working

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,24 @@
+import { useStateValue } from '../StateProvider'
 import '../styles/Product.css'
 
 const Product = ({ id, title, image, price, rating }) => {
+    const [{ basket }, dispatch] = useStateValue()
+
+    console.log(basket)
+
+    const addToBasket = () => {
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            }
+        })
+    }
+
     return(
         <div className='product'>
             <div className='product_info'>
@@ -21,7 +39,7 @@ const Product = ({ id, title, image, price, rating }) => {
                 src={image} 
                 alt='' 
             />
-            <button>Add To Basket</button>
+            <button onClick={addToBasket}>Add To Basket</button>
         </div>
     )
 }
