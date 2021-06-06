ce840080759c33a4acc84ca8a472993a41b30952
ihposey12 <ihposey@crimson.ua.edu>
2021-06-06 16:27:19 -0700

    finished and deployed with firebase-node-express

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,32 @@
 const functions = require("firebase-functions");
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
-const stripe = require('stripe')('sk_test_51IzDeHGDtIBo4KBLCIkmCVoXfBVvXjKWTj7Thrk9F69ftsHcTGKa2vtNod9GUb19LjnSx9HWsQXt9zMp6h5pznJU00RBMUcQz1')
+const stripe = require('stripe')('sk_test_51IzDeHGDtIBo4KBLCIkmCVoXfBVvXjKWTj7Thrk9F69ftsHcTGKa2vtNod9GUb19LjnSx9HWsQXt9zMp6h5pznJU00RBMUcQz1');
 
 //API SETUP
 
 
 //App config
-const app = express()
+const app = express();
 
 //Middlewares
-app.use(cors({ origin: true }))
-app.use(express.json())
+app.use(cors({ origin: true }));
+app.use(express.json());
 
 //API Routes
-app.get('/', (req, res) => res.status(200).send('hello world'))
+app.get('/', (req, res) => res.status(200).send('hello world'));
 
-app.post('/payments/create', async (req, res) => {
-    const total = req.query.total
-    console.log('Payment Request Received', total)
+app.post('/payments/create', async (request, response) => {
+    const total = request.query.total
     const paymentIntent = await stripe.paymentIntents.create({
         amount: total,
-        currency: 'usd',
-    })
+        currency: "usd",
+    });
 
-    res.status(201).send({
+    response.status(201).send({
         clientSecret: paymentIntent.client_secret,
-    }) //OK - created something
-})
+    });
+});
 
 //Listen Command
-exports.api = functions.https.onRequest(app)
+exports.api = functions.https.onRequest(app);
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-51973/us-central1/api'
+    baseURL: 'https://us-central1-clone-51973.cloudfunctions.net/api'
 })
 
 export default instance
+
+//http://localhost:5001/clone-51973/us-central1/api FOR TESTING
+
